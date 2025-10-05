fd678ce639f1082a0a0ee2c544a57f282542b5de
mechanicalcoders <121671478+mechanicalcoders@users.noreply.github.com>
2025-10-05 22:03:00 +0530

    c

--- app.py
+++ app.py
@@ -27,7 +27,9 @@ def split_rollno(table):
         new_row = []
         for num, cell in enumerate(row):
             if cell:
-                match = re.match(r"^(\d{5,})\s+(.+)$", cell.strip())
+                text = str(cell).strip()
+                text = re.sub(r"\s+", " ", text)
+                match = re.match(r"^(\d{5,})\s+(.+)$", text)
                 if match:
                     # print(match.groups())
                     rollno, name_parent = match.groups()
@@ -46,6 +48,52 @@ def split_rollno(table):
     return split_table
 
 
+def row_contains_rollno(row):
+    match = "";
+    for num, cell in enumerate(row):
+        if cell:
+            text = str(cell).strip()
+            #             print(text, "text")
+            text = re.sub(r"\s+", " ", text)
+            match = re.match(r"^(\d{5,})\s+(.+)$", text)
+            if match:
+                return match
+    return match
+
+
+def merge_rows(table):
+    merged_table = []
+    i = 0
+    while i < len(table):
+        current_row = table[i]  # get current row
+        if bool(row_contains_rollno(current_row)):  # check if row has roll no
+            merged_row = current_row.copy()
+            merg_count = 0
+            for k in range(
+                1, 3
+            ):  # check if next row has roll no or not if not merge next row
+                if i + k < len(table):
+                    next_row = table[i + k]
+                    if not bool(row_contains_rollno(next_row)):
+                        #                 print("no")
+                        for j in range(len(current_row)):
+                            if next_row[j]:  #!= None or next_row[j]!=""):
+                                merged_row[j] = next_row[j]
+                        print("no")
+                        merg_count += 1
+                    else:
+                        break
+                else:
+                    break
+            merged_table.append(merged_row)
+            i += 1 + merg_count
+        else:
+            merged_table.append(current_row)
+            #         print("Hello", i)
+            i += 1
+    return merged_table
+
+
 def read_pdf(path_to_pdf):
     pdf = pdfplumber.open(path_to_pdf)
     print(pdf)
@@ -54,7 +102,8 @@ def read_pdf(path_to_pdf):
     for page_num, page in enumerate(pdf.pages):
         tb = page.extract_table({"text_line_dir": "btt"})
         if tb:
-            cleaned_table = clean_table(tb)
+            merge_table = merge_rows(tb)
+            cleaned_table = clean_table(merge_table)
             df = pd.DataFrame(cleaned_table)
             all_tables.append(df)
     return all_tables
@@ -67,75 +116,64 @@ def write_files(table, filename):
     combined_df.to_csv(f"{filename}.csv", index=False, header=False)
 
 
+def get_folder(This_folder):
+    # This_folder = os.walk("./")
+    print(This_folder)
+    Input_path = []
+    for dirpath, dirname, filenames in This_folder:
+        # print(dirpath, "\n", dirname, "\n", filenames)
+        # print(filenames)
+        if dirpath == "./Files":
+            print(dirpath, filenames)
+            for dir in dirname:
+                Input_path.append(os.path.join(dirpath, dir))
+    print(Input_path)
+    return Input_path
+
+
+def read_files_path(Input_path):
+    print(Input_path[-1], "yes")
+    counter = 0
+    for path in Input_path:
+        path = Input_path[-1]
+        files = os.listdir(path)
+        counter = counter + 1
+        print(files)
+        print(counter)
+        if files:
+            Output_path = []
+            for file in files:
+                if file.endswith(".pdf"):
+                    path_to_pdf = os.path.join(path, file)
+                    print(path_to_pdf, "jo")
+                    table = read_pdf(path_to_pdf)
+                    Out_path = (
+                        path_to_pdf.split("/")[0]
+                        + "/Results/"
+                        + path_to_pdf.split("/")[2]
+                    )
+                    os.makedirs(Out_path, exist_ok=True)
+                    # print((path_to_pdf.split("/")[3]).split("."))
+                    Output_filename = (
+                        Out_path + "/" + (path_to_pdf.split("/")[3]).split(".")[0]
+                    )
+                    write_files(table, Output_filename)
+                    Output_path.append(f"{Output_filename}.csv")
+            json_file = (
+                path_to_pdf.split("/")[0]
+                + "/Results/"
+                + path_to_pdf.split("/")[2]
+                + "/"
+                + "index.json"
+            )
+            print(json_file)
+            # for input in Input_path:
+            with open(json_file, "w") as f:
+                json.dump({"Files": Output_path}, f, indent=2)
+
+
 This_folder = os.walk("./")
-print(This_folder)
-Input_path = []
-Output_path = []
-for dirpath, dirname, filenames in This_folder:
-    print(dirpath, "\n", dirname, "\n", filenames)
-    # print(filenames)
-    if dirpath == "./Files":
-        print(dirpath, filenames)
-        for dir in dirname:
-            Input_path.append(os.path.join(dirpath, dir))
-
-print(Input_path)
-# print(Output_path)
-counter = 0
-for path in Input_path:
-    files = os.listdir(path)
-    counter = counter + 1
-    print(files)
-    print(counter)
-    if files:
-        Output_path = []
-        for file in files:
-            if file.endswith(".pdf"):
-                path_to_pdf = os.path.join(path, file)
-                print(path_to_pdf, "jo")
-                table = read_pdf(path_to_pdf)
-                Output_path = (
-                    path_to_pdf.split("/")[0] + "/Results/" + path_to_pdf.split("/")[2]
-                )
-                os.makedirs(Output_path, exist_ok=True)
-                # print((path_to_pdf.split("/")[3]).split("."))
-                Output_filename = (
-                    Output_path + "/" + (path_to_pdf.split("/")[3]).split(".")[0]
-                )
-                write_files(table, Output_filename)
-        break
-        json_file = (
-            path_to_pdf.split("/")[0]
-            + "/Results/"
-            + path_to_pdf.split("/")[2]
-            + "/"
-            + "index.json"
-        )
-        print(json_file)
-        # for input in Input_path:
-        with open(json_file, "w") as f:
-            json.dump({"Files": Output_path}, f, indent=2)
-#     print(os.listdir(input))
-# filename = os.path.join(THIS_FOLDER, path_to_pdf)
-# file = This_folder[5]
-# path_to_pdf = "./Files/" + file
-# pdf = pdfplumber.open(path_to_pdf)
-# all_tables = []
-# print(path_to_pdf)
-
-
-# print(tables)
-# rows = []
-# print("TABLE")
-# for table in tables:
-#     print(table)
-#     for row in table:
-#         rows.append(row)
-#         # print(row)
-#     break;
-#     print("\\n")
-
-
+read_files_path(get_folder(This_folder))
 # @app.route("/")
 # def home():
 #     return render_template("index.html")
