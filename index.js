aef38de9cd2723f62c7dcdbd4024dcdfbe6aef09
mechanicalcoders <121671478+mechanicalcoders@users.noreply.github.com>
2025-10-07 18:18:21 +0530

    c

--- index.js
+++ index.js
@@ -1,111 +1,43 @@
-async function getDirectory(roll) {
-    // const INput_files = { folder_name: ['004_Resultsheet_Aug2021', '004_Resultsheet_Jan2021', '004_Resultsheet_Dec2016', '004_Resultsheet_Dec2017', '004_Resultsheet_Dec2018', '004_Resultsheet_Dec2019', '004_Resultsheet_June2019', '004_Resultsheet_Feb2022', '004_Resultsheet_July2022', '004_Resultsheet_Feb2023'] }
-    // console.log(INput_files)
-    const url_folder = 'https://api.github.com/repos/oiexam/oiexam.github.io/contents/Files/'
-    let response = await fetch(url_folder, { headers: { 'Authorization': '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' } });
-    let folders = await response.json();
-    console.log(folders);
-    for (const folder of folders) {
-        const url_files = 'https://api.github.com/repos/oiexam/oiexam.github.io/contents/Files/' + folder.name;
-        let res_file = await fetch(url_files, { headers: { 'Authorization': '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' } });
-        let files = await res_file.json();
-        for (const file of files) {
-            const file_name = '/Files/' + folder.name + '/' + file.name;
-            const roll_no = roll;
-            await getItems(file_name, roll_no);
-        }
-    }
-}
-async function getItems(src, roll_no) {
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '/build/pdf.worker.js';
-    const doc = await pdfjsLib.getDocument(src).promise
-    let pages = [];
-    for (page_num = 1; page_num <= doc.numPages; page_num++) {
-        const page = await doc.getPage(page_num)
-        const content = await page.getTextContent()
-        const items = content.items
-        if (items.length < 10) {
-            continue
-        }
-        let single_page_Arr = []; // single page content
-        for (var i = 0; i < items.length - 1; i++) {
-            if (isBlank(items[i].str)) {
-                continue
-            }
-            else {
-                if (isNumber(items[i].str) && items[i].str.length == 12) {
-                    if (items[i].str == roll_no) {
-                        pages.push(page_num);
-                    }
-                }
-            }
-        }
-    }
-    if (pages.length != 0) {
-        console.log(pages)
-        const viewer = document.getElementById('pdf-viewer');
-        for (const page_num of pages) {
-            canvas = document.createElement("canvas");
-            canvas.className = 'pdf-page-canvas';
-            viewer.appendChild(canvas);
-            renderPage(page_num, canvas);
-        }
-    }
-    const scale = 1.8;
+let csvData = [];
+let dataLoaded = false;
+window.onload = async function () {
+  await loadCSVData();
+  console.log(csvData);
+  disPlayTable
+};
 
-    function renderPage(num, canvas) {
-        doc.getPage(num).then(page => {
-            var viewport = page.getViewport({ scale });
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            const renderCtx = {
-                canvasContext: canvas.getContext('2d'),
-                viewport: viewport
-            }
-            page.render(renderCtx);
+async function loadCSVData(forceReload = false) {
+  const fil = "004_Resultsheet_Aug2021";
+  const url = "./Results/" + fil + "/index.json";
+  const response = await fetch(url);
+  console.log(response);
+  const file_list = await response.json();
+  console.log(file_list.Files);
+  if (dataLoaded && !forceReload) {
+    return csvData;
+  }
+
+  console.log("Loading Csv...");
+  const promises = file_list.Files.map(
+    (file) =>
+      new Promise((resolve, reject) => {
+        Papa.parse(file, {
+          download: true,
+          header: false, // Since your file has irregular/multi-line headers
+          skipEmptyLines: true,
+          complete: (result) => {
+            dataLoaded = true;
+            console.log("CSV loaded: ");
+            resolve(result.data);
+            console.log(result.data);
+          },
+          error: (err) => {
+            console.error("Error Loading CSV");
+            reject(err);
+          },
         });
-    }
-    function isBlank(str) {
-        return (!str || /^\s*$/.test(str));
-    }
-    function isNumber(str) {
-        return !isNaN(Number(str));
-    }
-    function isEmpty(obj) {
-        return (Object.keys(obj).length === 0);
-    }
-    return
-}
-document.querySelector("#close").addEventListener("click", function () {
-    document.querySelector(".popup").style.display = "none";
-});
-function onlyNumberkey(evt) {
-    var ass = (evt.which) ? evt.which : evt.keyCode
-    if (ass > 31 && (ass < 48 || ass > 57))
-        return false;
-    return true;
-}
-async function showresult(evt) {
-    const rollno = evt.target.previousElementSibling;
-    if (rollno.value == "" || rollno.value.length < 12) {
-        rollno.focus();
-    } else {
-        delete_child();
-        document.getElementById("loader").style.display = 'flex';
-        await getDirectory(rollno.value);
-        if (document.getElementById("pdf-viewer").childElementCount == 0) {
-            document.querySelector(".popup").firstChild.innerHTML = "No Result Found"
-        } else {
-            document.querySelector(".popup").firstChild.innerHTML = "Result is Processed Successfully"
-        }
-        document.getElementById("loader").style.display = 'none';
-        document.querySelector(".popup").style.display = "block";
-        document.getElementById("pdf-viewer").style.display = 'block';
-    }
-}
-function delete_child() {
-    const div = document.getElementById("pdf-viewer");
-    while (div.childElementCount > 0) {
-        div.removeChild(div.lastChild);
-    }
+      })
+  );
+  const allResults = await Promise.all(promises);
+  csvData = allResults.flat();
 }
--- static/js/mech.js
+++ static/js/mech.js
@@ -1,17 +1,104 @@
-window.onload = function () {
-  console.log("laoded");
-  fetch_file();
+let csvData = [];
+let dataLoaded = false;
+window.onload = async function () {
+  await loadCSVData();
+  console.log(csvData);
 };
-// Select the branch on click
-let data_file = [];
+
+// function to loadcsv data
+async function loadCSVData(forceReload = false) {
+  const fil = document.querySelector(".selected").innerText;
+  const url = "./Results/" + fil + "/index.json";
+  const response = await fetch(url);
+  console.log(response.ok);
+  if (!response.ok) {
+    throw new Error("File not found");
+  }
+  const file_list = await response.json();
+  console.log(file_list.Files);
+  if (dataLoaded && !forceReload) {
+    return csvData;
+  }
+
+  console.log("Loading Csv...");
+  const promises = file_list.Files.map(
+    (file) =>
+      new Promise((resolve, reject) => {
+        Papa.parse(file, {
+          download: true,
+          header: false, // Since your file has irregular/multi-line headers
+          skipEmptyLines: true,
+          complete: (result) => {
+            dataLoaded = true;
+            console.log("CSV loaded: ");
+            resolve(result.data);
+          },
+          error: (err) => {
+            console.error("Error Loading CSV");
+            reject(err);
+          },
+        });
+      })
+  );
+  const allResults = await Promise.all(promises);
+  csvData = allResults.flat();
+}
 
 // function to select the branch
-function branch_select(e) {
+async function branch_select(e) {
   console.log(e);
   e.preventDefault;
   const selected = document.querySelector(".selected");
+  const dropdown = document.getElementsByClassName("dropdown")[0];
   selected.innerText = e.textContent;
-  fetch_file();
+  dropdown.classList.toggle("clicked");
+  dropdown.querySelector("ul").scrollTop = 0;
+  await loadCSVData(true);
+  console.log("Branch Selected", e.textContent);
+  console.log(csvData);
+}
+const dropdown = document.getElementsByClassName("dropdown")[0];
+dropdown.addEventListener("mouseleave", () => {
+  if (dropdown.classList.contains("clicked")) {
+    dropdown.classList.remove("clicked");
+  }
+});
+
+function showresult(evt) {
+  delete_table();
+  console.log(evt);
+  const rollno = evt.target.previousElementSibling;
+  if (rollno.value.length < 7) {
+    rollno.focus();
+  } else {
+    create_data(rollno.value);
+  }
+}
+
+function create_data(value) {
+  let final_result = [];
+  console.log(value);
+  // console.log(data_file);
+  if (csvData.length > 0) {
+    csvData.forEach((row) => {
+      for (const cell of row) {
+        if (cell === value) {
+          const f = row.slice(1, 3).concat(row.slice(-3));
+          final_result.push(f);
+          console.log(row);
+          return;
+        }
+      }
+    });
+  }
+  console.log(final_result);
+  delete_table();
+  for (const j of final_result) {
+    if (j) {
+      create_table(j);
+      console.log(j);
+    }
+  }
 }
 
 // function to create the table of a student for the input roll no
@@ -61,7 +148,6 @@ function delete_table() {
     document.getElementsByClassName("table_container")[0].firstElementChild;
   if (tbl.childElementCount == 2) {
     tr = tbl.children[1];
-    console.log(tr);
     while (tr.hasChildNodes()) {
       tr.removeChild(tr.firstChild);
     }
@@ -73,82 +159,7 @@ function delete_table() {
   }
 }
 
-function create_data(value) {
-  let final_result = [];
-  console.log(value);
-  console.log(data_file);
-  if (data_file.length > 0) {
-    data_file.forEach((element) => {
-      let f = read_data(element, value);
-      if (f.length > 0) {
-        if (final_result.length == 0) {
-          final_result.push(f); // store if not empty
-        } else if (f.length > 1) {
-          // console.log(f);
-          // console.log(f.slice(1));
-          final_result.push(f.slice(1));
-        }
-      }
-      // break;                 // stop loop if you want first match
-    });
-  }
-  console.log(final_result);
-  if (final_result.length > 1) {
-    for (const result of final_result) {
-      console.log(result);
-      create_table(result[0]);
-    }
-  }
-}
-
 // show resutls function
-function showresult(evt) {
-  delete_table();
-  const rollno = evt.target.previousElementSibling;
-  if (rollno.value == "" || rollno.value.length < 12) {
-    rollno.focus();
-  } else {
-    create_data(rollno.value);
-  }
-}
-
-// file fetch function
-function fetch_file() {
-  const fil = document.querySelector(".selected").innerText;
-  const url = "./Results/" + fil;
-  fetch(url + "/index.json")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("File not found");
-      }
-      return res.json();
-    })
-    .then((file) => {
-      console.log(file.Files);
-      read_file(file.Files);
-    })
-    .catch((error) => {
-      console.error("Error Fetching", error);
-    });
-}
-
-// file read function
-function read_file(input) {
-  for (const input_file of input) {
-    Papa.parse(input_file, {
-      header: false, // Since your file has irregular/multi-line headers
-      download: true,
-      skipEmptyLines: true,
-      // newline: "\n", // Let PapaParse auto-detect newlines
-      complete: function (results) {
-        data_file.push(results.data);
-      },
-      error: function (error) {
-        console.error("Error parsing CSV:", error);
-      },
-    });
-  }
-}
 
 // function to read data for specific roll no.
 function read_data(data, value) {
